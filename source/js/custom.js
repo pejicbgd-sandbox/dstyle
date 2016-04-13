772c4343dad6d93a3f1bd6bdbe77cab6fd5f9158
Ivan Pejic <pejicbgd@hotmail.com>
2016-04-13 20:53:45 +0200

    Validacija forme

--- source/js/custom.js
+++ source/js/custom.js
@@ -49,13 +49,14 @@
 		}, 1000);
 
 	});
+	
 	$('.price-icons').find('i').hover(function() {
-        var $this = $(this);
-        $this.toggleClass("rotate");
+		var $this = $(this);
+		$this.toggleClass("rotate");
 	}).mouseenter(function() {
 		var $this = $(this),
-			holder = $('.price-content').find('h1'),
-		    target = $this.prev(); 
+		holder = $('.price-content').find('h1'),
+		target = $this.prev(); 
 
 		if(target.hasClass('haircut')) 
 		{
@@ -120,169 +121,175 @@
 	});
 
 	$(window).bind('mousewheel', function(e) {
-    if (e.originalEvent.wheelDelta < 0 && $(window).scrollTop() == 0) {
-        $('html, body').stop().animate({
-			scrollTop: $('#usluge').position().top - 7
-		}, 600);
-    }
-});
+		if (e.originalEvent.wheelDelta < 0 && $(window).scrollTop() == 0) {
+			$('html, body').stop().animate({
+				scrollTop: $('#usluge').position().top - 7
+			}, 600);
+		}
+	});
+
+	$('.fa-map-marker').click(function() {
+		$('html, body').stop().animate({
+				scrollTop: $('#map').position().top
+			}, 600);
+	});
 
 })(jQuery);
 
 google.maps.event.addDomListener(window, 'load', initWindow);
 function initWindow()
 {
-    var map,
-        bounds = new google.maps.LatLngBounds(),
-        mapOptions = {
-            mapTypeId: 'roadmap',
-            zoom: 16
-        };
-				var styles = [
-				    {
-				        "featureType": "landscape",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "lightness": 65
-				            },
-				            {
-				                "visibility": "on"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "poi",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "lightness": 51
-				            },
-				            {
-				                "visibility": "simplified"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "road.highway",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "visibility": "simplified"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "road.arterial",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "lightness": 30
-				            },
-				            {
-				                "visibility": "on"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "road.local",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "lightness": 40
-				            },
-				            {
-				                "visibility": "on"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "transit",
-				        "stylers": [
-				            {
-				                "saturation": -100
-				            },
-				            {
-				                "visibility": "simplified"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "administrative.province",
-				        "stylers": [
-				            {
-				                "visibility": "off"
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "water",
-				        "elementType": "labels",
-				        "stylers": [
-				            {
-				                "visibility": "on"
-				            },
-				            {
-				                "lightness": -25
-				            },
-				            {
-				                "saturation": -100
-				            }
-				        ]
-				    },
-				    {
-				        "featureType": "water",
-				        "elementType": "geometry",
-				        "stylers": [
-				            {
-				                "hue": "#ffff00"
-				            },
-				            {
-				                "lightness": -25
-				            },
-				            {
-				                "saturation": -97
-				            }
-				        ]
-				    }
-				],
-    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	var map,
+	bounds = new google.maps.LatLngBounds(),
+	mapOptions = {
+		mapTypeId: 'roadmap',
+		zoom: 16
+	};
+	var styles = [
+	{
+		"featureType": "landscape",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"lightness": 65
+		},
+		{
+			"visibility": "on"
+		}
+		]
+	},
+	{
+		"featureType": "poi",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"lightness": 51
+		},
+		{
+			"visibility": "simplified"
+		}
+		]
+	},
+	{
+		"featureType": "road.highway",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"visibility": "simplified"
+		}
+		]
+	},
+	{
+		"featureType": "road.arterial",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"lightness": 30
+		},
+		{
+			"visibility": "on"
+		}
+		]
+	},
+	{
+		"featureType": "road.local",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"lightness": 40
+		},
+		{
+			"visibility": "on"
+		}
+		]
+	},
+	{
+		"featureType": "transit",
+		"stylers": [
+		{
+			"saturation": -100
+		},
+		{
+			"visibility": "simplified"
+		}
+		]
+	},
+	{
+		"featureType": "administrative.province",
+		"stylers": [
+		{
+			"visibility": "off"
+		}
+		]
+	},
+	{
+		"featureType": "water",
+		"elementType": "labels",
+		"stylers": [
+		{
+			"visibility": "on"
+		},
+		{
+			"lightness": -25
+		},
+		{
+			"saturation": -100
+		}
+		]
+	},
+	{
+		"featureType": "water",
+		"elementType": "geometry",
+		"stylers": [
+		{
+			"hue": "#ffff00"
+		},
+		{
+			"lightness": -25
+		},
+		{
+			"saturation": -97
+		}
+		]
+	}
+	],
+	map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-    var infoWindow = new google.maps.InfoWindow(),
-        countrymarker;
+	var infoWindow = new google.maps.InfoWindow(),
+	countrymarker;
 
-    countrymarker = new google.maps.Marker({
-        position: new google.maps.LatLng(44.819909, 20.470312),
-        map: map,
-        title: "Salon Lepote DStyle"
-    });
+	countrymarker = new google.maps.Marker({
+		position: new google.maps.LatLng(44.819909, 20.470312),
+		map: map,
+		title: "Salon Lepote DStyle"
+	});
 
-    bounds.extend(countrymarker.position);
+	bounds.extend(countrymarker.position);
 
-    google.maps.event.addListener(countrymarker, 'click', (function(countrymarker) {
-        return function() {
-            infoWindow.setContent('<div class="info-content"><h4>Salon Lepote Dstyle</h4><p>Jelisavete Načić 20<br>11000 Beograd<br>+381 62 252-080<br>+381 63 252-081</p></div>');
-            infoWindow.open(map, countrymarker);
-        }
-    })(countrymarker));
+	google.maps.event.addListener(countrymarker, 'click', (function(countrymarker) {
+		return function() {
+			infoWindow.setContent('<div class="info-content"><h4>Salon Lepote Dstyle</h4><p>Jelisavete Načić 20<br>11000 Beograd<br>+381 62 252-080<br>+381 63 252-081</p></div>');
+			infoWindow.open(map, countrymarker);
+		}
+	})(countrymarker));
 
-    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event)
-	    {
-	    	this.setZoom(16);
-	        google.maps.event.removeListener(boundsListener);
-	    });
+	var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event)
+	{
+		this.setZoom(16);
+		google.maps.event.removeListener(boundsListener);
+	});
 
-    map.fitBounds(bounds);
-    map.setOptions({styles: styles});
+	map.fitBounds(bounds);
+	map.setOptions({styles: styles});
 }
 
